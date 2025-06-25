e39b8605685541dd9d28a6d091a67e249c532a6a
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2025-06-25 12:14:33 -0700

    troubleshooting

--- src/image_ingestion_service/image_receiver/app/ftp.py
+++ src/image_ingestion_service/image_receiver/app/ftp.py
@@ -12,11 +12,18 @@ async def upload_to_ftp(image_bytes: bytes, filename: str, camera_id: str):
     user = os.getenv("FTP_USER", "test")
     password = os.getenv("FTP_PASS", "test")
     target_dir = os.getenv("FTP_TARGET_DIR", "")
-    async with aioftp.Client.context(host, port, user, password, passive=True) as client:
+
+    client = aioftp.Client()
+    client.passive = True
+
+    try:
+        await client.connect(host, port)
+        await client.login(user, password)
         logger.info(f"Connected to FTP server {host}:{port} as user {user}")
+
         try:
             await client.change_directory(target_dir)
-        except aioftp.StatusCodeError as e:
+        except aioftp.StatusCodeError:
             await client.make_directory(target_dir)
             await client.change_directory(target_dir)
 
@@ -34,5 +41,34 @@ async def upload_to_ftp(image_bytes: bytes, filename: str, camera_id: str):
 
         # Clean up
         Path(tmp_file_path).unlink()
+        
+    except Exception as e:
+        logger.error(f"Push to FTP failed from {camera_id}: {e}")
+    finally:
+        await client.quit()
+        tmp_file_path.unlink(missing_ok=True)
+
+    # async with aioftp.Client.context(host, port, user, password) as client:
+    #     logger.info(f"Connected to FTP server {host}:{port} as user {user}")
+    #     try:
+    #         await client.change_directory(target_dir)
+    #     except aioftp.StatusCodeError as e:
+    #         await client.make_directory(target_dir)
+    #         await client.change_directory(target_dir)
+
+    #     # Save to a temp file
+    #     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+    #         tmp_file.write(image_bytes)
+    #         tmp_file_path = Path(tmp_file.name)
+    #         remote_path = f"{camera_id}/{filename}"   
+        
+    #     tmp_file_path.rename(filename)
+        
+    #     # Upload the renamed file to FTP
+    #     await client.upload(tmp_file_path, remote_path, write_into=True)
+    #     logger.info(f"Uploaded {filename} to FTP server at {remote_path}")        
+
+    #     # Clean up
+    #     Path(tmp_file_path).unlink()
 
 
