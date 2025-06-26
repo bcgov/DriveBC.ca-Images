cd6f89178149269fc542d08ea039b87b209486aa
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2025-06-26 13:28:00 -0700

    troubleshooting

--- src/image_ingestion_service/image_receiver/app/ftp.py
+++ src/image_ingestion_service/image_receiver/app/ftp.py
@@ -18,13 +18,14 @@ async def upload_to_ftp(image_bytes: bytes, filename: str, camera_id: str) -> bo
     ftp_client.passive = True
 
     try:
+        print(f"{host}:{port}:{user}:{password}:{target_dir}")
         await ftp_client.connect(host, port)
         await ftp_client.login(user, password)
         logger.info(f"Connected to FTP server {host}:{port} as user {user}")
 
         # Ensure target directory exists
         try:
-            print(f"{host}:{port}:{user}:{password}:{target_dir}")
+            
             await ftp_client.change_directory(target_dir)
         except aioftp.StatusCodeError:
             await ftp_client.make_directory(target_dir)
