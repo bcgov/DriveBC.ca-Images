7b8f3c1d9cdbfebfb975b6cbc81f6bf560a81e87
bcgov-brwang <87880048+bcgov-brwang@users.noreply.github.com>
2025-08-05 08:24:57 -0700

    removed init sql for creating image_index table

--- src/image_ingestion_service/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE image_index (
-    id SERIAL PRIMARY KEY,
-    camera_id TEXT NOT NULL,
-    original_pvc_path TEXT,
-    watermarked_pvc_path TEXT,
-    original_s3_path TEXT,
-    watermarked_s3_path TEXT,
-    timestamp TIMESTAMPTZ NOT NULL
-);
